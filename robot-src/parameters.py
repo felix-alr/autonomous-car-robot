7a323ef07f29757c9c5882f307ac7aa425971215
Julius Fiedler <ichderjulius@gmx.de>
2025-10-24 10:26:52 +0200

    cosmetic change

--- robot-src/parameters.py
+++ robot-src/parameters.py
@@ -2,8 +2,8 @@
 # Global definitions of physical parameters.
 
 ## wheel radius in mm
-ROBOT_WHEEL_RADIUS = 16.7
+ROBOT_WHEEL_RADIUS = 1 # todo students: set correct value
 ## distance between wheels in mm
-ROBOT_WHEEL_DISTANCE = 87.0
+ROBOT_WHEEL_DISTANCE = 1 # todo students: set correct value
 ## encoder counts per revolution (1:29.86 gearbox, 12 counts per motor shaft revolution)
-COUNTS_PER_REV = 358.32
+COUNTS_PER_REV = 1 # todo students: set correct value

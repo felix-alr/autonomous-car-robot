cbdc8329cdc793b32e19d5ca2f263023450a460b
xileFx <felix.amler@icloud.com>
2025-11-26 20:59:23 +0100

    First version of line follower

--- robot_src/control.py
+++ robot_src/control.py
@@ -110,48 +110,50 @@ class LineFollower:
         # 8) Store previous error
         self.prev_e = error
 
-    
 
-
-## Controller to attain a given movement of forward speed and turning rate.
-class KinematicController:
+## Controller for following the black parcours line.
+class LineFollower:
     def __init__(self, motors: Motors, perception: Perception):
         self._motors = motors
         self._perception = perception
 
-        self.forward_speed = 0
-        self.turn_speed = 0
-        self.manual_sensitivity_fwd = 100
-        self.manual_sensitivity_rot = 50
+        # PD gains (tune as needed)
+        # Actual 'functional' values
+        self.kp = 0.006
+        self.kd = 0.0000002
 
-    ## Set movement setpoint.
-    #
-    # @param v forward speed
-    # @param w turn rate
-    def set_vw(self, v: float, w: float):
-        pass
+        self.dt = 0.05
+        self.prev_e = 0
 
-    def increase_v(self):
-        self.forward_speed += self.manual_sensitivity_fwd
-        return self.forward_speed
-
-    def decrease_v(self):
-        self.forward_speed -= self.manual_sensitivity_fwd
-        return self.forward_speed
-
-    def increase_w(self):
-        self.turn_speed += self.manual_sensitivity_rot
-        return self.turn_speed
-
-    def decrease_w(self):
-        self.turn_speed -= self.manual_sensitivity_rot
-        return self.turn_speed
+        # Base forward PWM
+        self.duty_cycle = 0.12
+        self.v0 = 6000 * self.duty_cycle  # PWM
+        self.ctrl = 0
 
     def run(self):
-        self._motors.set_speeds(
-            self.forward_speed + self.turn_speed, self.forward_speed - self.turn_speed
-        )
+        # Get line deviation in mm
 
+        ## As the reference = 0, the error would be -(deviation).
+        error = -self._perception.get_line_deviation()
+        der = (error - self.prev_e) / self.dt  # mm/s
+
+        # PD output in mm/s : w
+        self.ctrl = self.kp * error + self.kd * der
+
+        # Motor PWM
+        left = self.v0 - self.ctrl
+        right = self.v0 + self.ctrl
+
+        # Clip just in case, use safety factor
+        safe = 0.9
+        left = max(min(left, 6000 * safe), -6000 * safe)
+        right = max(min(right, 6000 * safe), -6000 * safe)
+
+        # Send to motors
+        self._motors.set_speeds(left, right)
+
+        # Store previous error
+        self.prev_e = error
 
 ## Controller to follow a polynomial path between a start and target pose.
 class PathFollower:

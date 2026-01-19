f5be319836326f6fad8fbfaa1ca35604bbb935d5
xileFx <felix.amler@icloud.com>
2026-01-19 18:47:27 +0100

    Updated more generic version of test function for tuning

--- robot_test/control_test.py
+++ robot_test/control_test.py
@@ -1,11 +1,11 @@
 # This test file serves the purpose of testing specific features.
 
-def alter_parameters(uart, con, parameter):
+def alter_parameters(uart, clazz, parameter):
     uart.write(f"Enter value for {parameter}: \n")
     buf = uart.readline().decode("utf-8", "strict")
     val = float(buf.strip())
     try:
         uart.write(f"Value: '{val}'\n")
-        setattr(con.kinematic_controller, parameter, val)
+        setattr(clazz, parameter, val)
     except AttributeError:
         uart.write(f"Unknown attribute '{parameter}'. Could not assign value '{val}'.")
